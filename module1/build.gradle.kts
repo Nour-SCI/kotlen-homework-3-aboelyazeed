5e669f0ce4d8184afe3075574ebd4b942a4069bd
Aboelyazeed <abdelrahman.m.aboelyazeed@gmail.com>
2025-05-15 04:23:37 +0300

    update libraries and build successful

--- module1/build.gradle.kts
+++ module1/build.gradle.kts
@@ -1,6 +1,17 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
 group = rootProject.group
 version = rootProject.version
 
 dependencies {
-    project(":module2")
+    implementation(project(":module2"))
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.params)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
